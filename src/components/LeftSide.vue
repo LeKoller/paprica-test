6e53f18858ba0b92942886d8c29d96021d5169d4
Leandro Koller <lekoller@yahoo.com>
2021-05-06 00:39:39 -0300

    feat: Add right side basis.

---
+++ src/components/LeftSide.vue
@@ -0,0 +1,135 @@
+<template>
+  <main>
+    <div id="main_box">
+      <span id="open">OPEN TODAY FOR GREAT FOOD</span>
+      <span id="slogan">
+        You Can Make Your Own
+        <span class="yellow_chars">Burger</span>
+      </span>
+      <span id="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore
+      </span>
+      <div id="get_started_button"><span>GET STARTED</span></div>
+      <div id="about_box">
+        <span id="about_us" class="yellow_chars">About Us</span>
+        <span id="about_text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. mpor
+          incididunt utmpor
+        </span>
+        <div id="rod"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LeftSide",
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  #main_box {
+    z-index: 3;
+    width: 31vw;
+    height: 31.76vh;
+    margin-top: 18vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    span {
+      margin: 0;
+      padding: 0;
+    }
+
+    #open {
+      font-size: 22px;
+      font-family: "Open Sans", sans-serif;
+      letter-spacing: 5px;
+      color: #bbbbbb;
+      text-align: start;
+    }
+
+    #slogan {
+      margin-top: 3.4vh;
+      width: 600px;
+      font-family: "Modak", cursive;
+      font-size: 70px;
+      color: #0e0d2a;
+      line-height: 90%;
+      text-align: start;
+    }
+
+    #text {
+      width: 435px;
+      font-size: 14px;
+      margin-top: 2vh;
+      color: #bbbbbb;
+      text-align: start;
+    }
+
+    #get_started_button {
+      margin-top: 3vh;
+      border: 2px solid #0e0d2a;
+      color: #0e0d2a;
+      border-radius: 999px;
+      opacity: 0.4;
+      padding: 17px 30px;
+      cursor: pointer;
+      transition: all 0.25s ease;
+
+      &:hover {
+        background-color: #101010;
+        color: #fff;
+      }
+
+      span {
+        font-size: 17px;
+        letter-spacing: 1px;
+      }
+    }
+
+    #about_box {
+      margin-top: 18vh;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      position: relative;
+
+      #about_us {
+        font-size: 19px;
+        font-weight: bold;
+      }
+
+      #about_text {
+        margin-top: 2.2vh;
+        width: 384px;
+        color: #bbbbbb;
+        font-size: 14px;
+        text-align: start;
+      }
+
+      #rod {
+        background-color: #0e0d2a;
+        width: 200px;
+        height: 7px;
+        position: absolute;
+        right: 416px;
+        top: 8px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="container">
     <div id="main_content">
-      <main>
-        <div id="main_box">
-          <span id="open">OPEN TODAY FOR GREAT FOOD</span>
-          <span id="slogan">
-            You Can Make Your Own
-            <span class="yellow_chars">Burger</span>
+      <LeftSide />
+      <div id="slider_container">
+        <img src="@/assets/burger.png" alt="Hamburguer" id="burger_pic" />
+        <div id="burger_case">
+          <span id="bur">BUR</span>
+          <span id="ger">GER</span>
+          <span id="flavor">
+            Spicy Beef <span class="yellow_chars">Burger</span>
           </span>
-          <span id="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore
-          </span>
-          <div id="get_started_button"><span>GET STARTED</span></div>
-          <div id="about_box">
-            <span id="about_us" class="yellow_chars">About Us</span>
-            <span id="about_text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. mpor
-              incididunt utmpor
-            </span>
-            <div id="rod"></div>
-          </div>
         </div>
-      </main>
-      <div id="slider_container"></div>
+        <div id="social_media">
+          <span id="dont_miss">Don’t Miss Our Promo On</span>
+        </div>
+      </div>
     </div>
 
     <span id="food">FOOD</span>
@@ -40,6 +30,7 @@ import { defineComponent } from "vue";
 import UpperLeft from "../components/shapes/UpperLeft.vue";
 import DownerLeft from "../components/shapes/DownerLeft.vue";
 import Right from "../components/shapes/Right.vue";
+import LeftSide from "../components/LeftSide.vue";
 
 export default defineComponent({
   name: "Home",
@@ -47,6 +38,7 @@ export default defineComponent({
     UpperLeft,
     DownerLeft,
     Right,
+    LeftSide,
   },
   setup() {},
 });
@@ -76,114 +68,70 @@ export default defineComponent({
     justify-content: space-between;
     width: 100%;
 
-    main {
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    #slider_container {
+      z-index: 2;
       width: 100%;
+      position: relative;
 
-      #main_box {
-        z-index: 3;
-        width: 31vw;
-        height: 31.76vh;
-        margin-top: 18vh;
+      #burger_case {
+        width: 532px;
+        height: 666px;
+        position: absolute;
+        right: 12vw;
+        top: 7.2vh;
         display: flex;
         flex-direction: column;
+        justify-content: space-between;
         align-items: flex-start;
 
-        span {
+        #bur,
+        #ger {
+          z-index: 3;
+          font-family: "Archivo Black", sans-serif;
+          font-size: 230px;
+          color: transparent;
+          -webkit-text-stroke: 2px #b9b9b9;
+          opacity: 0.4;
           margin: 0;
           padding: 0;
         }
 
-        #open {
-          font-size: 22px;
-          font-family: "Open Sans", sans-serif;
-          letter-spacing: 5px;
-          color: #bbbbbb;
-          text-align: start;
-        }
-
-        #slogan {
-          margin-top: 3.4vh;
-          width: 600px;
+        #flavor {
+          z-index: 5;
+          position: absolute;
+          font-size: 49px;
           font-family: "Modak", cursive;
-          font-size: 70px;
-          color: #0e0d2a;
-          line-height: 90%;
-          text-align: start;
+          left: 70px;
+          bottom: 56px;
         }
+      }
 
-        #text {
-          width: 435px;
-          font-size: 14px;
-          margin-top: 2vh;
-          color: #bbbbbb;
-          text-align: start;
-        }
-
-        #get_started_button {
-          margin-top: 3vh;
-          border: 2px solid #0e0d2a;
-          color: #0e0d2a;
-          border-radius: 999px;
-          opacity: 0.4;
-          padding: 17px 30px;
-          cursor: pointer;
-          transition: all 0.25s ease;
-
-          &:hover {
-            background-color: #101010;
-            color: #fff;
-          }
-
-          span {
-            font-size: 17px;
-            letter-spacing: 1px;
-          }
-        }
-
-        #about_box {
-          margin-top: 18vh;
-          display: flex;
-          flex-direction: column;
-          align-items: flex-start;
-          position: relative;
-
-          #about_us {
-            font-size: 19px;
-            font-weight: bold;
-          }
-
-          #about_text {
-            margin-top: 2.2vh;
-            width: 384px;
-            color: #bbbbbb;
-            font-size: 14px;
-            text-align: start;
-          }
-
-          #rod {
-            background-color: #0e0d2a;
-            width: 200px;
-            height: 7px;
-            position: absolute;
-            right: 416px;
-            top: 8px;
-          }
-        }
+      #burger_pic {
+        z-index: 4;
+        top: 16.5vh;
+        right: 7.74vw;
+        -webkit-filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.1));
+        filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.1));
+        position: absolute;
       }
     }
 
-    #slider_container {
-      width: 100%;
+    #social_media {
+      position: absolute;
+      top: calc(90vh - 135px);
+      left: 264px;
+
+      #dont_miss {
+        font-size: 19px;
+        opacity: 0.7019607843137254;
+      }
     }
   }
 
   .shape {
     z-index: 1;
-    -webkit-filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.12));
-    filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.12));
+    -webkit-filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.1));
+    filter: drop-shadow(0px 0px 16px rgba(0, 0, 0, 0.1));
   }
 
   .upper_left {
